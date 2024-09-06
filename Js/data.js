2e9a6ec9233fb419eabbae9f660cd287825ac7e6
Ph-Alves <ph.costaalves@gmail.com>
2024-09-06 13:41:53 -0300

    Alteração no footer + data

--- Js/data.js
+++ Js/data.js
@@ -1,3 +1,13 @@
-let criaturas = {
-    
-};
+let criaturas = [
+    {
+        nome: "Goblin",
+        tipo: "Monstro",
+        descricao: "Goblins são criaturas pequenas e verdes, conhecidas por sua covardia e crueldade.",
+    },
+    {
+
+    },
+    {
+
+    },
+];
